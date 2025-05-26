6723703bfff715e6565adea72246ffcfdbd37c5b
Tutku Ince <tutku.ince@outlook.com>
2025-05-26 21:56:02 +0300

    Service class has been updated with using repository

--- src/main/java/com/incetutku/ecom/UserService.java
+++ src/main/java/com/incetutku/ecom/UserService.java
@@ -1,41 +1,38 @@
 package com.incetutku.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private List<User> userList = new ArrayList<>();
-    private Long nextId = 1L;
+
+    private final UserRepository userRepository;
 
     public void addUser(User user) {
-        user.setId(nextId++);
-        userList.add(user);
+        userRepository.save(user);
     }
 
     public boolean updateUser(Long id, User updatedUser) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
+        return userRepository.findById(id)
                 .map(existingUser -> {
                     existingUser.setName(updatedUser.getName());
                     existingUser.setSurname(updatedUser.getSurname());
+                    userRepository.save(existingUser);
                     return true;
                 }).orElse(false);
     }
 
     public List<User> fetchAllUsers() {
-        return userList;
+        return userRepository.findAll();
     }
 
     public Optional<User> fetchUserById(Long id) {
-        return userList.stream().filter(user -> user.getId().equals(id)).findFirst();
+        return userRepository.findById(id);
     }
-
-
 
 
 }
