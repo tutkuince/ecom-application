4b6994aa829ab8cc219e5211183142ff1389a3c2
Tutku Ince <tutku.ince@outlook.com>
2025-05-26 21:39:33 +0300

    Entity

--- src/main/java/com/incetutku/ecom/User.java
+++ src/main/java/com/incetutku/ecom/User.java
@@ -1,12 +1,26 @@
 package com.incetutku.ecom;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String surname;
 }
