a6c8916512a0f3b173a234c6a505a9e1dc7e9ae1
Tutku Ince <tutku.ince@outlook.com>
2025-05-27 22:00:17 +0300

    Product entity and Product Repository

---
+++ src/main/java/com/incetutku/ecom/model/Product.java
@@ -0,0 +1,33 @@
+package com.incetutku.ecom.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stockQuantity;
+    private String category;
+    @Column(name = "image_url")
+    private String imageUrl;
+    private Boolean isActive = true;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
