dd7fd8a4034987fa583fdf5eaeac45edac1994ce
Tutku Ince <tutku.ince@outlook.com>
2025-05-28 22:07:45 +0300

    new method

--- src/main/java/com/incetutku/ecom/repository/ProductRepository.java
+++ src/main/java/com/incetutku/ecom/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.incetutku.ecom.repository;
 import com.incetutku.ecom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByIsActiveTrue();
 }
