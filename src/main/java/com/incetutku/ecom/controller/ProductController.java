5cb8ec6e39c6f509f3ae7a60ac9cc1c605d42903
Tutku Ince <tutku.ince@outlook.com>
2025-05-28 21:46:26 +0300

    create product service and api

---
+++ src/main/java/com/incetutku/ecom/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.incetutku.ecom.controller;
+
+import com.incetutku.ecom.dto.ProductRequest;
+import com.incetutku.ecom.dto.ProductResponse;
+import com.incetutku.ecom.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        return new ResponseEntity<>(productService.createProduct(productRequest), HttpStatus.CREATED);
+    }
+}
+
---
+++ src/main/java/com/incetutku/ecom/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.incetutku.ecom.service;
+
+import com.incetutku.ecom.dto.ProductRequest;
+import com.incetutku.ecom.dto.ProductResponse;
+import com.incetutku.ecom.model.Product;
+import com.incetutku.ecom.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = new Product();
+        updateProductFromProductRequest(product, productRequest);
+        Product savedProduct = productRepository.save(product);
+        return mapToProductResponse(savedProduct);
+    }
+
+    private ProductResponse mapToProductResponse(Product savedProduct) {
+        ProductResponse response = new ProductResponse();
+        response.setId(savedProduct.getId());
+        response.setName(savedProduct.getName());
+        response.setDescription(savedProduct.getDescription());
+        response.setPrice(savedProduct.getPrice());
+        response.setStockQuantity(savedProduct.getStockQuantity());
+        response.setCategory(savedProduct.getCategory());
+        response.setImageUrl(savedProduct.getImageUrl());
+        response.setIsActive(savedProduct.getIsActive());
+        return response;
+    }
+
+    private void updateProductFromProductRequest(Product product, ProductRequest productRequest) {
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+        product.setStockQuantity(productRequest.getStockQuantity());
+        product.setCategory(productRequest.getCategory());
+        product.setImageUrl(productRequest.getImageUrl());
+    }
+}
