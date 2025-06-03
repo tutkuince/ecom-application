24db94a1b0897f94c2b598753b00776892a1b434
Tutku Ince <tutku.ince@outlook.com>
2025-06-03 19:04:58 +0300

    create order at OrderController

---
+++ src/main/java/com/incetutku/ecom/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.incetutku.ecom.controller;
+
+import com.incetutku.ecom.dto.OrderResponse;
+import com.incetutku.ecom.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> createOrder(@RequestHeader("X-User-ID") String userId) {
+        return orderService.createOrder(userId)
+                .map(orderResponse -> new ResponseEntity<>(orderResponse, HttpStatus.CREATED))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
