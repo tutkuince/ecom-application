42d3b4ceb9207626233c04f9d32e789096d518a5
Tutku Ince <tutku.ince@outlook.com>
2025-06-03 17:08:22 +0300

    dto

---
+++ src/main/java/com/incetutku/ecom/dto/OrderItemDTO.java
@@ -0,0 +1,13 @@
+package com.incetutku.ecom.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class OrderItemDTO {
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+    private BigDecimal price;
+}
---
+++ src/main/java/com/incetutku/ecom/dto/OrderResponse.java
@@ -0,0 +1,17 @@
+package com.incetutku.ecom.dto;
+
+import com.incetutku.ecom.model.OrderStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class OrderResponse {
+    private Long id;
+    private BigDecimal totalAmount;
+    private OrderStatus status;
+    private List<OrderItemDTO> items;
+    private LocalDateTime createdAt;
+}
