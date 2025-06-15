84b94d36382030573c5bd1a13f1f2b33f70e192c
Tutku Ince <tutku.ince@outlook.com>
2025-06-15 22:36:50 +0300

    changed for mongo db

--- user-service/src/main/java/com/incetutku/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/incetutku/userservice/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.incetutku.userservice.repository;
 
 import com.incetutku.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, Long> {
 }
