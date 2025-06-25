ce0ef26a7fed350aa800cecc5a0820b4ac1bf3d4
Tutku Ince <tutku.ince@outlook.com>
2025-06-25 20:59:12 +0300

    updated for mongodb

--- user-service/src/main/java/com/incetutku/userservice/model/User.java
+++ user-service/src/main/java/com/incetutku/userservice/model/User.java
@@ -1,12 +1,8 @@
 package com.incetutku.userservice.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -21,24 +17,15 @@ public class User implements Serializable {
     @Id
     private String id;
 
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private String surname;
-
     @Indexed(unique = true)
     private String email;
-
-    @Column(name = "mobile_number")
     private String mobileNumber;
-
     private UserRole role = UserRole.CUSTOMER;
     private Address address;
-
     @CreatedDate
     private LocalDateTime createdAt;
-
     @LastModifiedDate
     private LocalDateTime updatedAt;
 }
--- user-service/src/main/java/com/incetutku/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/incetutku/userservice/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.incetutku.userservice.repository;
 
 import com.incetutku.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, Long> {
